4bcd88f4f172696783fe720fde6ea90277c970a0
unknown <solihin.solehin@gmail.com>
2026-02-07 01:39:59 +0800

    Initial MaiSinggah foundation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>MaiSinggah.my</h1>
+      <p>
+        Mai singgah jap. Kami tapis barang Shopee Malaysia yang
+        betul-betul berguna, bukan sekadar murah.
+      </p>
+    </main>
+  )
+}
